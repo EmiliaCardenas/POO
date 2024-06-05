f3fb7109e823339084aede78b6f2c433c4ec466a
EmiliaCardenas <142023291+EmiliaCardenas@users.noreply.github.com>
2024-06-05 17:33:06 -0600

    Cuentafinal.h

--- Cuentafinal.h
+++ Cuentafinal.h
@@ -10,14 +10,18 @@ using namespace std;
 class Cuentafinal{
   private:
     Cliente * cliente[20];
+    Envioinfo * envio[20];
     int no;
 
   public:
     Cuentafinal(): no(0){};
 
     void registros_clientes();
+    void registro_envio();
     void ver_registros();
+    void ver_envios();
     void ver_registros(string clase);
+    void ver_envios(string metodo);
 };
 
 void Cuentafinal::registros_clientes(){
@@ -30,6 +34,19 @@ void Cuentafinal::registros_clientes(){
   cliente[no] = new Kpop("Sofia", "A01701475", "Corea", "Lightstick Enhypen", "Kpop", 1500, 2);
   no++;
   cliente[no] = new Kpop("Emilia", "A01706807", "Mexico", "Pcs Han Jisung", "Kpop", 120, 298);
+  no++;
+  cliente [no] = new Anime ("Zoe", "A01706734", "China", "Peluche Pochita", "Anime", 350, 3);
+  no++;
+}
+
+void Cuentafinal::registro_envio(){
+  envio [no] = new Envioinfo (657493598,4,6,2023,37,20,"aereo");
+  no++;
+  envio [no] = new Envioinfo (629748156,5,7,2024,43,10,"terrestre");
+  no++;
+  envio [no] = new Envioinfo (318165488,6,8,2026,12,9,"marino");
+  no++;
+  envio [no] = new Envioinfo (218432184,7,9,2027,6,18,"aereo");
   no++;
 }
 
@@ -47,5 +64,18 @@ void Cuentafinal::ver_registros(string clase){
   }
 }
 
+void Cuentafinal::ver_envios(){
+  for (int i = 0; i < no; i++){
+    envio[i]->anadir_envio();
+  }
+}
+
+void Cuentafinal::ver_envios(string metodo){
+  for (int i = 0; i < no; i++){
+    if (envio[i] -> getMetodo() == metodo){
+      envio[i]->anadir_envio();
+    }
+  }
+}
 
 #endif
