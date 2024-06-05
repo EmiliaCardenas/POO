fae032e7f2a744c9eae38dc9ca7d7597a193ed4d
EmiliaCardenas <142023291+EmiliaCardenas@users.noreply.github.com>
2024-06-05 17:32:35 -0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -7,18 +7,68 @@
 using namespace std;
 
 int main() {
-  cout << "\n\n ------Todos los registros------" << endl;
-  Cuentafinal cliente;
-  cliente.registros_clientes();
-  cliente.ver_registros();
+  int n, k;
+  cout << "Bienvenidx al registro de compras, elige una opcion." << endl;
+  cout << "---Ver registros  1" << endl;
+  cout << "---Ver envios     2" << endl;
+  cout << "Cual es tu eleccion? " << endl;
+  cin >> k;
 
-  cout << "\n\n ------Imprimir registro de anime------" << endl;
-  cliente.ver_registros("Anime");
+  if (k == 1){
+    cout << "---Todos  1" << endl;
+    cout << "---Anime  2" << endl;
+    cout << "---Kpop   3" << endl;
+    cout << "Cuales registros quieres ver? " << endl;
+    cin >> n;
 
-  cout << "\n\n ------Imprimir registro de kpop------" << endl;
-  cliente.ver_registros("Kpop");
+    Cuentafinal cliente;
+    cliente.registros_clientes();
+
+    if (n == 1) {
+      cout << "\n\n ------Todos los registros------" << endl;
+      cliente.ver_registros();
+    }
+
+    if (n == 2) {
+    cout << "\n\n ------Imprimir registro de anime------" << endl;
+    cliente.ver_registros("Anime");
+    }
+
+    if (n == 3) {
+    cout << "\n\n ------Imprimir registro de kpop------" << endl;
+    cliente.ver_registros("Kpop");
+    }
+  }
   
-  Envioinfo envio1(657493598,4,7,2024,37,20,"avión");
-  envio1.anadir_envio();
+  if (k == 2){
+    cout << "---Todos      1" << endl;
+    cout << "---Terrestre  2" << endl;
+    cout << "---Aereo      3" << endl;
+    cout << "---Marino     4" << endl;
+    cout << "Cuales registros quieres ver? " << endl;
+    cin >> n;
+    Cuentafinal envio1;
+    envio1.registro_envio();
+
+    if (n == 1) {
+      cout << "\n\n ------Imprimir envios------" << endl;
+      envio1.ver_envios();
+    }
+
+    if (n == 2) {
+      cout << "\n\n ------Imprimir envios Terrestres------" << endl;
+      envio1.ver_envios("terrestre");
+    }
+
+    if (n == 3) {
+      cout << "\n\n ------Imprimir envios Aereos------" << endl;
+      envio1.ver_envios("aereo");
+    }
+
+    if (n == 4) {
+      cout << "\n\n ------Imprimir envios Marinos------" << endl;
+      envio1.ver_envios("marino");
+    }
+  }
 
 }
