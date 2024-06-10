5134f18309488e2d68f08ce5f3ebe177203ea24e
EmiliaCardenas <142023291+EmiliaCardenas@users.noreply.github.com>
2024-06-10 16:53:43 -0600

    Cuentafinal.h

--- Cuentafinal.h
+++ Cuentafinal.h
@@ -1,3 +1,14 @@
+/*
+Proyecto Tienda de anime y kpop
+Emilia Cárdenas Lobatón
+A01706807
+13/06/2024
+
+Esta clase define los objetos de las clases hijas de Cliente y Orden
+incluyendo sus metodos
+es para crea obejtos de Anime, Kpop y Envioinfo
+*/
+
 #ifndef CUENTAFINAL_H
 #define CUENTAFINAL_H
 #include<iostream>
@@ -7,14 +18,15 @@
 using namespace std;
 
 class Cuentafinal{
-  private:
-    Cliente * cliente[50];
-    Envioinfo * envio[50];
+  private: // atributos y variables privadas
+    Cliente * cliente[50]; // apuntador para polimorfismo
+    Envioinfo * envio[50]; // apuntador para polimorfismo
     int no;
 
   public:
-    Cuentafinal(): no(0){};
+    Cuentafinal(): no(0){}; // constructor default
 
+    // metodos
     void registros_clientes();
     void registro_envio();
     void ver_registros();
@@ -23,7 +35,18 @@ class Cuentafinal{
     void ver_envios(string metodo);
 };
 
+/*
+registros_clientes genera objetos de tipo Cliente[]
+
+genera objetos de prueba de Anime y Kpop
+los guarda en la variable cliente[] (arreglo de clientes)
+son unicamente datos falsos y demostrativos
+
+@param
+@return
+*/
 void Cuentafinal::registros_clientes(){
+  // uso de new para crear objetos con polimorfismo
   cliente [no] = new Anime ("Emilia", "A01706807", "Mexico", "Manga Banana Fish", "Anime", 150, 10);
   no++;
   cliente[no] = new Kpop("Victoria", "A01708200", "Japon", "BTS Album", "Kpop", 500, 30);
@@ -38,7 +61,18 @@ void Cuentafinal::registros_clientes(){
   no++;
 }
 
+/*
+registros_envio genera objetos de tipo Envioinfo[]
+
+genera objetos de prueba de Envioinfo
+los guarda en la variable envio[] (arreglo de envios)
+son unicamente datos falsos y demostrativos
+
+@param
+@return
+*/
 void Cuentafinal::registro_envio(){
+  // uso de new para crear objetos con polimorfismo
   envio [no] = new Envioinfo (657493598,4,6,2023,37,20,"aereo");
   no++;
   envio [no] = new Envioinfo (629748156,5,7,2024,43,10,"terrestre");
@@ -51,12 +85,31 @@ void Cuentafinal::registro_envio(){
   no++;
 }
 
+/*
+ver_registros imprime los clientes
+
+utilizando el arreglo cliente[] y no, recorre todo el arreglo
+e imprime cada uno utilizando el metodo info_productos
+
+@param
+@return
+*/
 void Cuentafinal::ver_registros(){
   for (int i = 0; i < no; i++){
     cliente[i]->info_productos();
   }
 }
 
+/*
+ver_registros imprime los clientes coincidiendo en la clase
+
+utilizando el arreglo cliente[] y no, recorre todo el arreglo
+e imprime cada uno utilizando el metodo info_productos
+string clase ('Anime' o 'Kpop')
+
+@param string clase debe ser: 'Anime' o 'Kpop'
+@return
+*/
 void Cuentafinal::ver_registros(string clase){
   for (int i = 0; i < no; i++){
     if (cliente[i] -> getClase() == clase){
@@ -65,12 +118,31 @@ void Cuentafinal::ver_registros(string clase){
   }
 }
 
+/*
+ver_envios imprime los envios
+
+utilizando el arreglo envio[] y no, recorre todo el arreglo
+e imprime cada uno utilizando el metodo anadir_envio
+
+@param
+@return
+*/
 void Cuentafinal::ver_envios(){
   for (int i = 0; i < no; i++){
     envio[i]->anadir_envio();
   }
 }
 
+/*
+ver_envios imprime los envios coincidiendo en el metodo
+
+utilizando el arreglo envio[] y no, recorre todo el arreglo
+e imprime cada uno utilizando el metodo anadir_envio
+string metodo ('aereo', 'terrestre' o 'marino')
+
+@param string metodo debe ser: 'aereo', 'terrestre' o 'marino'
+@return
+*/
 void Cuentafinal::ver_envios(string metodo){
   for (int i = 0; i < no; i++){
     if (envio[i] -> getMetodo() == metodo){
@@ -78,6 +150,5 @@ void Cuentafinal::ver_envios(string metodo){
     }
   }
 }
-
 
 #endif
